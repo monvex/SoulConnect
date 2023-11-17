d8e2e27e7942f852c4b9d0c04fa0e8da17a83939
Anton Kononchenko <akononche@gmail.com>
2023-11-17 09:49:26 +0300

    Added design from figma via relay for start page. First version

--- app/src/main/java/com/example/soulconnect/MainActivity.kt
+++ app/src/main/java/com/example/soulconnect/MainActivity.kt
@@ -2,12 +2,29 @@ package com.example.soulconnect
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.ui.Modifier
+import com.example.soulconnect.startpage.StartPage
+import com.example.soulconnect.ui.theme.SoulConnectTheme
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+            SoulConnectTheme {
+                Column(
+                    modifier = Modifier
+                ) {
+                    StartPage(
+                        logInText = "Вход",
+                        signUpText = "Регистрация"
+                        )
+                }
+            }
+        }
     }
+
 
     override fun onStart() {
         super.onStart()
@@ -24,5 +41,6 @@ class MainActivity : AppCompatActivity() {
     override fun onDestroy() {
         super.onDestroy()
     }
+
 
 }
